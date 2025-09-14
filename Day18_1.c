9a612f3c4c5d033b2cc57fb59752ef4b5b993b6f
Shivam-joshii <shivam.jooshii@gmail.com>
2025-09-14 14:45:09 +0530

    Day18_1.c

---
+++ Day18_1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num ;
+    int i;
+    printf("Enter a number to find factors ");
+    scanf("%d", &num);
+    
+    for ( i = 1 ; i <=num ; i++)
+    {
+        if (num%i==0)
+        {
+            printf("%d \n" , i);
+        }
+        
+    }
+    return 0;
+}
