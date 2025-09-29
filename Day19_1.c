bf1a17b96c8254cf7e9a735bbd0a9c09485f32c8
Shivam-joshii <shivam.jooshii@gmail.com>
2025-09-29 16:46:38 +0530

    Day19_1.c

---
+++ Day19_1.c
@@ -0,0 +1,18 @@
+// Write a program to find LCM of two numbers.
+
+#include <stdio.h>
+int main() {
+    int a, b, i, max, lcm;
+    printf(" Enter a number:");
+    scanf("%d %d", &a, &b);
+    max = (a > b) ? a : b;
+    for (i = max; ; i++) {
+        if (i % a == 0 && i % b == 0) {
+            lcm = i;
+            break;
+        }
+    }
+    printf("%d", lcm);
+
+    return 0;
+}
