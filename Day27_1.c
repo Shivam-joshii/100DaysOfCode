85985c51a6c0338bff1894d7d59002534c143d2d
Shivam-joshii <shivam.jooshii@gmail.com>
2025-10-06 13:34:19 +0530

    Day27_1.c

--- Day27_1.c
+++ Day27_1.c
@@ -13,28 +13,19 @@
 #include <stdio.h>
 
 int main() {
-    int i, j, n = 5; // n is the number of rows for the top half
+    int n = 5; // height of the upper half
+    int i, j;
 
-    // upper part of the diamond
+    // Upper half
     for (i = 1; i <= n; i++) {
-        // print spaces
-        for (j = 1; j <= n - i; j++) {
-            printf(" ");
-        }
-        // print stars
         for (j = 1; j <= 2 * i - 1; j++) {
             printf("*");
         }
         printf("\n");
     }
 
-    // lower part of the diamond
+    // Lower half
     for (i = n - 1; i >= 1; i--) {
-        // print spaces
-        for (j = 1; j <= n - i; j++) {
-            printf(" ");
-        }
-        // print stars
         for (j = 1; j <= 2 * i - 1; j++) {
             printf("*");
         }
