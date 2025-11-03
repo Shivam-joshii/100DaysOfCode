dadb0530c307814796c9b2a26562a8ee8398d388
Shivam-joshii <shivam.jooshii@gmail.com>
2025-11-03 19:02:47 +0530

    Implement array reversal without extra space

---
+++ Day31_2.c
@@ -0,0 +1,36 @@
+//Reverse an array without taking extra space
+#include <stdio.h>
+
+void reverseArray(int arr[], int n) {
+    int start = 0;
+    int end = n - 1;
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    reverseArray(arr, n);
+
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
