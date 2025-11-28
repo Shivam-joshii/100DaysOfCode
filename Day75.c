1b19260297b6e0c4d81f55fe20d0e230f2c9bd46
Shivam-joshii <shivam.jooshii@gmail.com>
2025-11-28 22:16:15 +0530

    Create Day75.c

---
+++ Day75.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    fp = fopen("info.txt", "a");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+    
+    char line[256];
+    printf("Enter a line of text to append:\n");
+    getchar(); // to consume newline left by previous input
+    fgets(line, sizeof(line), stdin);
+    
+    fprintf(fp, "%s", line);
+    fclose(fp);
+    
+    printf("Text appended successfully\n");
+    return 0;
+}
