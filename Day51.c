493bcce737e5857be648e6eb2c086bfedd8d3bf3
Shivam-joshii <shivam.jooshii@gmail.com>
2025-11-28 23:53:16 +0530

    Update Day51.c

--- Day51.c
+++ Day51.c
@@ -1,44 +1,37 @@
 #include <stdio.h>
 
-void findFirstLast(int nums[], int size, int target, int *first, int *last) {
+void findFirstAndLast(int nums[], int n, int target, int *first, int *last) {
     *first = -1;
     *last = -1;
-
-    for (int i = 0; i < size; i++) {
-        if (nums[i] == target) {
-            *first = i;
-            break;
-        }
-    }
-
-    if (*first == -1) {
-        return;
-    }
-
-    for (int j = size - 1; j >= 0; j--) {
-        if (nums[j] == target) {
-            *last = j;
-            break;
+    
+    for(int i = 0; i < n; i++) {
+        if(nums[i] == target) {
+            if(*first == -1) {
+                *first = i;
+            }
+            *last = i;
         }
     }
 }
 
 int main() {
-    int nums1[] = {5,7,7,8,8,10};
-    int size = sizeof(nums1) / sizeof(nums1[0]);
-    int target = 8;
-
+    int n, target;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    
+    int nums[n];
+    printf("Enter sorted array elements: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+    
+    printf("Enter target: ");
+    scanf("%d", &target);
+    
     int first, last;
-    findFirstLast(nums1, size, target, &first, &last);
+    findFirstAndLast(nums, n, target, &first, &last);
+    
     printf("%d,%d\n", first, last);
-
-    target = 6;
-    findFirstLast(nums1, size, target, &first, &last);
-    printf("%d,%d\n", first, last);
-
-    target = 10;
-    findFirstLast(nums1, size, target, &first, &last);
-    printf("%d,%d\n", first, last);
-
+    
     return 0;
 }
