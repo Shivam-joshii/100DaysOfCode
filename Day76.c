32a10fb0284bb90030873dbbb7602b014e588526
Shivam-joshii <shivam.jooshii@gmail.com>
2025-11-28 22:17:35 +0530

    Add file reading program in Day76.c
    This program reads a filename from user input, opens the file in read mode, and prints its contents to the console.

---
+++ Day76.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char filename[100];
+    scanf("%s", filename);
+    
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Error: File does not exist\n");
+        return 1;
+    }
+    
+    char ch;
+    while ((ch = fgetc(fp)) != EOF) {
+        putchar(ch);
+    }
+    
+    fclose(fp);
+    return 0;
+}
